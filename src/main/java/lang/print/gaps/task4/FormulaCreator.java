9fb15dc5c43a4f4b0070b0c4d135013407a4c64e
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-17 19:20:28 +0200

    not all done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        char a = 97;
+        char b = 98;
+        int result = (9 * (a ^ 2)- 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
     }
 }
